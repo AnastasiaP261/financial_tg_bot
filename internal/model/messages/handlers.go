4d16da6fd24f82d5dda4c2e0c29f2ac5c4517826
apetrichuk <apetrichuk@ozon.ru>
2022-10-07 01:28:03 +0300

    [GOHW-1] правки - IncomingMessage разнесен на функции-обработчики

---
+++ internal/model/messages/handlers.go
@@ -0,0 +1,52 @@
+package messages
+
+import (
+	"github.com/pkg/errors"
+	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/model/purchases"
+)
+
+func (m *Model) msgReport(msg Message) error {
+	res := report.FindStringSubmatch(msg.Text)
+	if len(res) < 2 {
+		return m.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
+	}
+
+	period, err := m.purchasesModel.ToPeriod(res[1])
+	if err != nil {
+		return m.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
+	}
+
+	txt, img, err := m.purchasesModel.Report(period, msg.UserID)
+	if err != nil {
+		return m.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
+	}
+
+	if err = m.tgClient.SendMessage("Ваш отчет:\n\n"+txt, msg.UserID, msg.UserName); err != nil {
+		return err
+	}
+
+	return m.tgClient.SendImage(img, msg.ChatID, msg.UserName)
+}
+
+func (m *Model) msgAddCategory(msg Message) error {
+	res := addCategory.FindStringSubmatch(msg.Text)
+	if len(res) < 2 {
+		return m.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
+	}
+
+	err := m.purchasesModel.AddCategory(msg.UserID, res[1])
+	if err != nil {
+		return m.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
+	}
+	return m.tgClient.SendMessage(ScsTxtCategoryAdded, msg.UserID, msg.UserName)
+}
+
+func (m *Model) msgAddPurchase(msg Message, sum, category, date string) error {
+	if err := m.purchasesModel.AddPurchase(msg.UserID, sum, category, date); err != nil {
+		if errors.Is(err, purchases.ErrCategoryNotExist) {
+			return m.tgClient.SendMessage(ErrTxtCategoryDoesntExist, msg.UserID, msg.UserName)
+		}
+		return m.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
+	}
+	return m.tgClient.SendMessage(ScsTxtPurchaseAdded, msg.UserID, msg.UserName)
+}
--- internal/model/messages/incoming_msg.go
+++ internal/model/messages/incoming_msg.go
@@ -3,7 +3,6 @@ package messages
 import (
 	"regexp"
 
-	"github.com/pkg/errors"
 	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/model/purchases"
 )
 
@@ -39,12 +38,12 @@ type Message struct {
 }
 
 var (
-	// addConditionOnlySum сообщение о добавлении траты без категории и даты (указывается текущая дата)
-	addConditionOnlySum = regexp.MustCompile(`/add (\d+.?\d*)`)
-	// addConditionSumAndCategory сообщение о добавлении траты c категорией но без даты (указывается текущая дата)
-	addConditionSumAndCategory = regexp.MustCompile(`/add (\d+.?\d*) ([ \wФА-Яа-я]+)`)
-	// addConditionSumAndCategoryAndDate сообщение о добавлении траты c категорией и датой
-	addConditionSumAndCategoryAndDate = regexp.MustCompile(`/add (\d+\.?\d*) ([ \wФА-Яа-я]+) (\d{2}\.\d{2}\.\d{4})`)
+	// addPurchaseOnlySum сообщение о добавлении траты без категории и даты (указывается текущая дата)
+	addPurchaseOnlySum = regexp.MustCompile(`/add (\d+.?\d*)`)
+	// addPurchaseSumAndCategory сообщение о добавлении траты c категорией но без даты (указывается текущая дата)
+	addPurchaseSumAndCategory = regexp.MustCompile(`/add (\d+.?\d*) ([ \wФА-Яа-я]+)`)
+	// addPurchaseSumAndCategoryAndDate сообщение о добавлении траты c категорией и датой
+	addPurchaseSumAndCategoryAndDate = regexp.MustCompile(`/add (\d+\.?\d*) ([ \wФА-Яа-я]+) (\d{2}\.\d{2}\.\d{4})`)
 
 	// addCategory добавление новой категории
 	addCategory = regexp.MustCompile(`/category ([ \wФА-Яа-я\-]+)`)
@@ -53,91 +52,42 @@ var (
 	report = regexp.MustCompile(`/report (month|week|year)`)
 )
 
-func (s *Model) IncomingMessage(msg Message) error {
+func (m *Model) IncomingMessage(msg Message) error {
 	switch {
 	case msg.Text == "/start":
-		return s.tgClient.SendMessage("hello", msg.UserID, msg.UserName)
+		return m.tgClient.SendMessage("hello", msg.UserID, msg.UserName)
 
 	case report.MatchString(msg.Text):
-		res := report.FindStringSubmatch(msg.Text)
-		if len(res) < 2 {
-			return s.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
-		}
-
-		period, err := s.purchasesModel.ToPeriod(res[1])
-		if err != nil {
-			return s.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
-		}
-
-		txt, img, err := s.purchasesModel.Report(period, msg.UserID)
-		if err != nil {
-			return s.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
-		}
-
-		if err = s.tgClient.SendMessage("Ваш отчет:\n\n"+txt, msg.UserID, msg.UserName); err != nil {
-			return err
-		}
-
-		return s.tgClient.SendImage(img, msg.ChatID, msg.UserName)
+		return m.msgReport(msg)
 
 	case addCategory.MatchString(msg.Text):
-		res := addCategory.FindStringSubmatch(msg.Text)
-		if len(res) < 2 {
-			return s.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
-		}
+		return m.msgAddCategory(msg)
 
-		err := s.purchasesModel.AddCategory(msg.UserID, res[1])
-		if err != nil {
-			return s.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
-		}
-		return s.tgClient.SendMessage(ScsTxtCategoryAdded, msg.UserID, msg.UserName)
-
-	case addConditionSumAndCategoryAndDate.MatchString(msg.Text):
-		res := addConditionSumAndCategoryAndDate.FindStringSubmatch(msg.Text)
+	case addPurchaseSumAndCategoryAndDate.MatchString(msg.Text):
+		res := addPurchaseSumAndCategoryAndDate.FindStringSubmatch(msg.Text)
 		if len(res) < 4 {
-			return s.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
+			return m.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
 		}
 
-		err := s.purchasesModel.AddPurchase(msg.UserID, res[1], res[2], res[3])
-		if err != nil {
-			if errors.Is(err, purchases.ErrCategoryNotExist) {
-				return s.tgClient.SendMessage(ErrTxtCategoryDoesntExist, msg.UserID, msg.UserName)
-			}
-			return s.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
-		}
-		return s.tgClient.SendMessage(ScsTxtPurchaseAdded, msg.UserID, msg.UserName)
+		return m.msgAddPurchase(msg, res[1], res[2], res[3])
 
-	case addConditionSumAndCategory.MatchString(msg.Text):
-		res := addConditionSumAndCategory.FindStringSubmatch(msg.Text)
+	case addPurchaseSumAndCategory.MatchString(msg.Text):
+		res := addPurchaseSumAndCategory.FindStringSubmatch(msg.Text)
 		if len(res) < 3 {
-			return s.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
+			return m.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
 		}
 
-		err := s.purchasesModel.AddPurchase(msg.UserID, res[1], res[2], "")
-		if err != nil {
-			if errors.Is(err, purchases.ErrCategoryNotExist) {
-				return s.tgClient.SendMessage(ErrTxtCategoryDoesntExist, msg.UserID, msg.UserName)
-			}
-			return s.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
-		}
-		return s.tgClient.SendMessage(ScsTxtPurchaseAdded, msg.UserID, msg.UserName)
+		return m.msgAddPurchase(msg, res[1], res[2], "")
 
-	case addConditionOnlySum.MatchString(msg.Text):
-		res := addConditionOnlySum.FindStringSubmatch(msg.Text)
+	case addPurchaseOnlySum.MatchString(msg.Text):
+		res := addPurchaseOnlySum.FindStringSubmatch(msg.Text)
 		if len(res) < 2 {
-			return s.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
+			return m.tgClient.SendMessage(ErrTxtInvalidInput, msg.UserID, msg.UserName)
 		}
 
-		err := s.purchasesModel.AddPurchase(msg.UserID, res[1], "", "")
-		if err != nil {
-			if errors.Is(err, purchases.ErrCategoryNotExist) {
-				return s.tgClient.SendMessage(ErrTxtCategoryDoesntExist, msg.UserID, msg.UserName)
-			}
-			return s.tgClient.SendMessage("Ошибочка: "+err.Error(), msg.UserID, msg.UserName)
-		}
-		return s.tgClient.SendMessage(ScsTxtPurchaseAdded, msg.UserID, msg.UserName)
+		return m.msgAddPurchase(msg, res[1], "", "")
 
 	default:
-		return s.tgClient.SendMessage(ErrTxtUnknownCommand, msg.UserID, msg.UserName)
+		return m.tgClient.SendMessage(ErrTxtUnknownCommand, msg.UserID, msg.UserName)
 	}
 }
