9dfe0617a47fd1f25926ce35bdd694a2f5c71ead
apetrichuk <apetrichuk@ozon.ru>
2022-10-08 22:26:38 +0300

    [GOHW-2] клиент fixer переписан на фоновые запросы

--- internal/clients/fixer/client.go
+++ internal/clients/fixer/client.go
@@ -1,13 +1,18 @@
 package fixer
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/pkg/errors"
-	model "gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/model/exchange_rates"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 )
+
+const sleepTime = 30 * time.Minute
 
 // TokenGetter геттер токена апи
 type TokenGetter interface {
@@ -17,21 +22,47 @@ type TokenGetter interface {
 // Client fixer клиент
 type Client struct {
 	tokenGetter TokenGetter
+
+	exchangeRates map[string]float64
+	mutex         sync.RWMutex
 }
 
 // New создать инстанс клиента fixer
-func New(tokenGetter TokenGetter) *Client {
-	return &Client{tokenGetter: tokenGetter}
+func New(ctx context.Context, tokenGetter TokenGetter) *Client {
+	cl := &Client{tokenGetter: tokenGetter}
+
+	go func(ctx context.Context) {
+		for {
+			ctx, cancel := context.WithTimeout(ctx, time.Second*30)
+			defer cancel()
+
+			cl.process(ctx)
+			time.Sleep(sleepTime)
+		}
+	}(ctx)
+
+	return cl
 }
 
-// Response формат ответа
+func (c *Client) dataAccessorWrite(newData map[string]float64) {
+	c.mutex.Lock()
+	c.exchangeRates = newData
+	c.mutex.Unlock()
+}
+
+func (c *Client) dataAccessorRead() map[string]float64 {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	return c.exchangeRates
+}
+
+// Response формат ответа fixer
 type Response struct {
 	Success bool               `json:"success"`
 	Rates   map[string]float64 `json:"rates"`
 }
 
-// GetExchangeRateToRUB получить валютные курсы для USD, EUR и CNY к RUB
-func (c *Client) GetExchangeRateToRUB() (model.Response, error) {
+func (c *Client) process(ctx context.Context) {
 	u := url.URL{
 		Scheme: "https",
 		Host:   "api.apilayer.com",
@@ -44,9 +75,11 @@ func (c *Client) GetExchangeRateToRUB() (model.Response, error) {
 
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", u.String(), nil)
+	req.WithContext(ctx)
 	req.Header.Set("apikey", c.tokenGetter.FixerAPIToken())
 	if err != nil {
-		return model.Response{}, errors.Wrap(err, "http.NewRequest")
+		log.Printf("[FIXER CLIENT ERR]: %s\n", errors.Wrap(err, "http.NewRequest").Error())
+		return
 	}
 
 	res, err := client.Do(req)
@@ -55,14 +88,24 @@ func (c *Client) GetExchangeRateToRUB() (model.Response, error) {
 	}
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return model.Response{}, errors.Wrap(err, "ioutil.ReadAll")
+		log.Printf("[FIXER CLIENT ERR]: %s\n", errors.Wrap(err, "ioutil.ReadAll").Error())
+		return
 	}
 
 	var response Response
 	err = json.Unmarshal(body, &response)
 	if err != nil {
-		return model.Response{}, errors.Wrap(err, "unmarshalling error")
+		log.Printf("[FIXER CLIENT ERR]: %s\n", errors.Wrap(err, "unmarshalling error").Error())
 	}
 
-	return model.Response(response), nil
+	if response.Success {
+		c.dataAccessorWrite(response.Rates)
+	} else {
+		log.Println("[FIXER CLIENT ERR]: client request failed")
+	}
+}
+
+// GetExchangeRateToRUB получить валютные курсы для USD, EUR и CNY к RUB
+func (c *Client) GetExchangeRateToRUB() map[string]float64 {
+	return c.dataAccessorRead()
 }
