7e6acae304bd10fe5ebf8d4c54bb6619439906b1
apetrichuk <apetrichuk@ozon.ru>
2022-10-21 01:29:42 +0300

    [GOHW-3] готова миграция

--- migrations/00001_initial.sql
+++ migrations/00001_initial.sql
@@ -1,10 +1,44 @@
 -- +goose Up
 
-CREATE TABLE test
+CREATE TYPE currency AS ENUM ('RUS', 'EUR', 'USD', 'CNY');
+
+-- здесь автоматически создастся уникальный индекс для id
+CREATE TABLE users
 (
-    id bigserial PRIMARY KEY -- уникальный id
+    id   bigserial PRIMARY KEY NOT NULL, -- уникальный id юзера
+    curr currency              NOT NULL
 );
+
+CREATE TABLE categories
+(
+    id            bigserial PRIMARY KEY NOT NULL, -- уникальный id
+    user_id       bigint                NOT NULL,
+    category_name text                  NOT NULL
+);
+
+-- будем искать либо по id (но на него индекс создается автоматически),
+-- либо по паре юзер+категория, b-tree подходит
+CREATE UNIQUE INDEX categories_idx ON categories (user_id, category_name);
+
+CREATE TABLE purchases
+(
+    id          bigserial PRIMARY KEY NOT NULL, -- уникальный id
+    category_id bigint                NOT NULL, -- по этому айди можно узнать и юзера
+    sum         numeric               NOT NULL,
+    ts          timestamp             NOT NULL DEFAULT NOW(),
+
+    eur_ratio   numeric               NOT NULL,
+    usd_ratio   numeric               NOT NULL,
+    cny_ratio   numeric               NOT NULL
+);
+
+-- будем искать по категории и сравнивать с заданной датой, b-tree подходит
+CREATE INDEX purchases_idx ON purchases (category_id, ts);
 
 -- +goose Down
 
-DROP TABLE test;
+-- индексы удалятся автоматически при удалении таблиц
+DROP TABLE purchases;
+DROP TABLE categories;
+DROP TABLE users;
+DROP TYPE currency;
