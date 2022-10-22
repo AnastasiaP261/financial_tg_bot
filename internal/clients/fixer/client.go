dfa78daf4e7263ae833d0d4c5278566cfe9c2016
apetrichuk <apetrichuk@ozon.ru>
2022-10-22 16:01:00 +0300

    [GOHW-3] клиент может получать данные для указанной даты

--- internal/clients/fixer/client.go
+++ internal/clients/fixer/client.go
@@ -3,6 +3,7 @@ package fixer
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -79,6 +80,7 @@ type Response struct {
 	Rates   map[string]float64 `json:"rates"`
 }
 
+// getData получает из апи данные по курсу валют на текущий момент времени
 func (c *Client) getData(ctx context.Context) {
 	u := url.URL{
 		Scheme: "https",
@@ -128,7 +130,71 @@ func (c *Client) getData(ctx context.Context) {
 	}
 }
 
-// GetExchangeRateToRUB получить валютные курсы для USD, EUR и CNY к RUB
+// getDataFromDate получает из апи данные по курсу валют на указанную дату
+func (c *Client) getDataFromDate(ctx context.Context, y, m, d int) (map[string]float64, error) {
+	if y <= 1999 {
+		return nil, errors.New("invalid date")
+	}
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   "api.apilayer.com",
+		Path:   fmt.Sprintf("fixer/%d-%02d-%02d", y, m, d),
+		RawQuery: url.Values{
+			"access_key": []string{c.tokenGetter.FixerAPIToken()},
+			"base":       []string{"RUB"},
+			"symbols":    []string{"USD,EUR,CNY"},
+		}.Encode(),
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	req = req.WithContext(ctx)
+	req.Header.Set("apikey", c.tokenGetter.FixerAPIToken())
+	if err != nil {
+		return nil, errors.Wrap(err, "http.NewRequest")
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "client.Do")
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "ioutil.ReadAll")
+	}
+
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, errors.Wrap(err, "unmarshalling error")
+	}
+
+	if response.Success {
+		log.Printf("[FIXER]: получен курс для %02d.%02d.%d: %+v\n", d, m, y, response.Rates)
+		return response.Rates, nil
+	} else {
+		log.Println("[FIXER CLIENT ERR]: client request failed")
+		return nil, errors.New("client request failed with unknown error")
+	}
+}
+
+// GetExchangeRateToRUB получить последние валютные курсы для USD, EUR и CNY к RUB
 func (c *Client) GetExchangeRateToRUB() map[string]float64 {
 	return c.dataAccessorRead()
+}
+
+// GetExchangeRateToRUBFromDate получить валютные курсы для USD, EUR и CNY к RUB за конкретную дату
+func (c *Client) GetExchangeRateToRUBFromDate(ctx context.Context, y, m, d int) (map[string]float64, error) {
+	ctx, cancel := context.WithTimeout(ctx, fixerTimeout)
+	defer cancel()
+
+	res, err := c.getDataFromDate(ctx, y, m, d)
+	if err != nil {
+		return nil, errors.Wrap(err, "getDataFromDate")
+	}
+	return res, nil
 }
