2a60c928008a5603f7c568c22a6e986bd45f0525
apetrichuk <apetrichuk@ozon.ru>
2022-10-22 14:30:10 +0300

    [GOHW-3] lint fix

--- internal/model/db/purchases_test.go
+++ internal/model/db/purchases_test.go
@@ -13,17 +13,7 @@ func TestService_AddPurchase(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
-
-	start := func() (context.Context, *Service, func()) {
-		ctx := context.Background()
-		s, closeFunc := NewTestDB(ctx, t)
-
-		// заполнение необходимыми для теста данными
-		s.db.ExecContext(ctx, "INSERT INTO users (id, curr) VALUES (123, 'RUB')")                              // nolint:errcheck
-		s.db.ExecContext(ctx, "INSERT INTO categories (user_id, category_name) VALUES (123, 'some category')") // nolint:errcheck
-
-		return ctx, s, closeFunc
-	}
+	t.Parallel()
 
 	type purchase struct {
 		Sum        float64 `db:"sum"` // сумма траты в рублях
@@ -35,53 +25,31 @@ func TestService_AddPurchase(t *testing.T) {
 		EURRatio float64 `db:"eur_ratio"`
 	}
 
-	t.Run("создание траты с категорией", func(t *testing.T) {
-		t.Parallel()
-		ctx, s, close := start()
-		defer close()
+	ctx := context.Background()
+	s, close := NewTestDB(ctx, t)
+	defer close()
 
-		nowTime := time.Now()
-		err := s.AddPurchase(ctx, model.AddPurchaseReq{
-			UserID:     123,
-			Sum:        100,
-			CategoryID: 1,
-			Date:       nowTime,
-			USDRatio:   1,
-			CNYRatio:   1,
-			EURRatio:   1,
-		})
+	// заполнение необходимыми для теста данными
+	s.db.ExecContext(ctx, "INSERT INTO users (id, curr) VALUES (123, 'RUB')")                              // nolint:errcheck
+	s.db.ExecContext(ctx, "INSERT INTO categories (user_id, category_name) VALUES (123, 'some category')") // nolint:errcheck
 
-		assert.NoError(t, err)
-
-		// проверим что трата действительно создалась
-		var purchases []purchase
-		s.db.SelectContext(ctx, &purchases, "SELECT sum, category_id, usd_ratio, cny_ratio, eur_ratio  FROM purchases") // nolint:errcheck
-		assert.EqualValues(t, []purchase{{Sum: 100, CategoryID: 1, USDRatio: 1, CNYRatio: 1, EURRatio: 1}}, purchases)
+	nowTime := time.Now()
+	err := s.AddPurchase(ctx, model.AddPurchaseReq{
+		UserID:     123,
+		Sum:        100,
+		CategoryID: 1,
+		Date:       nowTime,
+		USDRatio:   1,
+		CNYRatio:   1,
+		EURRatio:   1,
 	})
 
-	t.Run("создание траты без категории", func(t *testing.T) {
-		t.Parallel()
-		ctx, s, close := start()
-		defer close()
+	assert.NoError(t, err)
 
-		nowTime := time.Now()
-		err := s.AddPurchase(ctx, model.AddPurchaseReq{
-			UserID:     123,
-			Sum:        100,
-			CategoryID: 0,
-			Date:       nowTime,
-			USDRatio:   1,
-			CNYRatio:   1,
-			EURRatio:   1,
-		})
-
-		assert.NoError(t, err)
-
-		// проверим что трата действительно создалась
-		var purchases []purchase
-		s.db.SelectContext(ctx, &purchases, "SELECT sum, category_id, usd_ratio, cny_ratio, eur_ratio  FROM purchases")
-		assert.EqualValues(t, []purchase{{Sum: 100, CategoryID: 0, USDRatio: 1, CNYRatio: 1, EURRatio: 1}}, purchases)
-	})
+	// проверим что трата действительно создалась
+	var purchases []purchase
+	s.db.SelectContext(ctx, &purchases, "SELECT sum, category_id, usd_ratio, cny_ratio, eur_ratio  FROM purchases") // nolint:errcheck
+	assert.EqualValues(t, []purchase{{Sum: 100, CategoryID: 1, USDRatio: 1, CNYRatio: 1, EURRatio: 1}}, purchases)
 }
 
 func TestService_GetUserPurchasesFromDate(t *testing.T) {
@@ -95,13 +63,14 @@ func TestService_GetUserPurchasesFromDate(t *testing.T) {
 	defer close()
 
 	// заполнение необходимыми для теста данными
-	s.db.ExecContext(ctx, "INSERT INTO users (id, curr) VALUES (123, 'RUB')")
-	s.db.ExecContext(ctx, "INSERT INTO categories (user_id, category_name) VALUES (123, 'some category 1'), (123, 'some category 2')")
-	s.db.ExecContext(ctx, `INSERT INTO purchases (category_id, sum, ts, eur_ratio, usd_ratio, cny_ratio) VALUES 
-                                                                                  (1, 100, '2022-09-27', 0.5, 0.5, 0.5), 
-                                                                                  (1, 200,'2022-10-01', 0.5, 0.5, 0.5), 
-                                                                                  (0, 300,'2022-10-06', 0.5, 0.5, 0.5), 
-                                                                                  (2, 400, '2022-10-24', 0.5, 0.5, 0.5)`)
+	s.db.ExecContext(ctx, "INSERT INTO users (id, curr) VALUES (123, 'RUB')")                                                          // nolint:errcheck
+	s.db.ExecContext(ctx, "INSERT INTO categories (user_id, category_name) VALUES (123, 'some category 1'), (123, 'some category 2')") // nolint:errcheck
+	q := `INSERT INTO purchases (category_id, sum, ts, eur_ratio, usd_ratio, cny_ratio) VALUES   
+                    (1, 100, '2022-09-27', 0.5, 0.5, 0.5), 
+                    (1, 200,'2022-10-01', 0.5, 0.5, 0.5), 
+                    (0, 300,'2022-10-06', 0.5, 0.5, 0.5), 
+                    (2, 400, '2022-10-24', 0.5, 0.5, 0.5)`
+	s.db.ExecContext(ctx, q) // nolint:errcheck
 
 	fromTime, _ := time.Parse("02.01.2006", "01.10.2022")
 	res, err := s.GetUserPurchasesFromDate(ctx, fromTime, 123)
