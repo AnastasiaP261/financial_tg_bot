3a745279baae54ebb989853de98058f46f1582e3
apetrichuk <apetrichuk@ozon.ru>
2022-11-13 21:26:18 +0300

    [GOHW-7] написан клиент бота для сервиса отчетов

--- internal/clients/financial-tg-bot/client.go
+++ internal/clients/financial-tg-bot/client.go
@@ -5,18 +5,29 @@ import (
 	"github.com/pkg/errors"
 	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/model/report"
 	pb "gitlab.ozon.dev/apetrichuk/financial-tg-bot/pkg/api/financial-tg-bot"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type service struct {
-	pb.MessagesServiceClient
+	cl pb.MessagesServiceClient
 }
 
-func New() *service {
-	return &service{}
+type configGetter interface {
+	GRPCHostMessages() string
+}
+
+func New(confService configGetter) (*service, error) {
+	conn, err := grpc.Dial(confService.GRPCHostMessages(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	return &service{cl: pb.NewMessagesServiceClient(conn)}, nil
 }
 
 func (s *service) SendReport(ctx context.Context, req report.SendReportRequest) (report.SendReportResponse, error) {
-	res, err := s.MessagesServiceClient.SendReport(ctx, &pb.SendReportRequest{
+	res, err := s.cl.SendReport(ctx, &pb.SendReportRequest{
 		UserId:        req.UserId,
 		ReportMessage: req.ReportMessage,
 	})
