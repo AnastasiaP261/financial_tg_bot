e2a62e151949d6c8771aa608b6fa305e8ce8223e
apetrichuk <apetrichuk@ozon.ru>
2022-11-13 20:57:33 +0300

    [GOHW-7] хранилище отчетов переписано на новый тип

--- internal/clients/redis/reports_store.go
+++ internal/clients/redis/reports_store.go
@@ -3,12 +3,12 @@ package redis
 import (
 	"context"
 	"encoding/json"
+	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/model/report"
 	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
 	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/logs"
-	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/model/purchases"
 	"gitlab.ozon.dev/apetrichuk/financial-tg-bot/internal/wrappers/metrics"
 	"go.uber.org/zap"
 )
@@ -40,23 +40,23 @@ func (r *Report) UnmarshalBinary(data []byte) error {
 	return nil
 }
 
-func (c *Client) SetReport(ctx context.Context, key string, value purchases.Report) error {
+func (c *Client) SetReport(ctx context.Context, key string, value report.Report) error {
 	items := make([]ReportItem, len(value.Items))
 	for i := range value.Items {
 		items[i] = ReportItem(value.Items[i])
 	}
 
-	report := Report{
+	r := Report{
 		Items:    items,
 		FromDate: value.FromDate,
 	}
 
 	var nullDur time.Duration
-	stCmd := c.rdb.Set(ctx, key, report, nullDur)
+	stCmd := c.rdb.Set(ctx, key, r, nullDur)
 
 	err := stCmd.Err()
 	if err != nil {
-		logs.Error("set report error", zap.Error(err))
+		logs.Error("set r error", zap.Error(err))
 		metrics.InFlightCache.WithLabelValues(metrics.StatusErr).Inc()
 		return err
 	}
@@ -65,29 +65,29 @@ func (c *Client) SetReport(ctx context.Context, key string, value purchases.Repo
 	return nil
 }
 
-func (c *Client) GetReport(ctx context.Context, key string) (purchases.Report, error) {
+func (c *Client) GetReport(ctx context.Context, key string) (report.Report, error) {
 	res := c.rdb.Get(ctx, key)
 	if errors.Is(res.Err(), redis.Nil) {
 		// здесь все ок так как такого значения просто нет в кеше
 		metrics.InFlightCache.WithLabelValues(metrics.StatusOk).Inc()
-		return purchases.Report{}, nil
+		return report.Report{}, nil
 	}
 
-	var report Report
-	if err := res.Scan(&report); err != nil {
-		logs.Error("set report error", zap.Error(err))
+	var r Report
+	if err := res.Scan(&r); err != nil {
+		logs.Error("set r error", zap.Error(err))
 		metrics.InFlightCache.WithLabelValues(metrics.StatusErr).Inc()
-		return purchases.Report{}, errors.Wrap(err, "scanning err")
+		return report.Report{}, errors.Wrap(err, "scanning err")
 	}
 	metrics.InFlightCache.WithLabelValues(metrics.StatusOk).Inc()
 
-	items := make([]purchases.ReportItem, len(report.Items))
-	for i := range report.Items {
-		items[i] = purchases.ReportItem(report.Items[i])
+	items := make([]report.ReportItem, len(r.Items))
+	for i := range r.Items {
+		items[i] = report.ReportItem(r.Items[i])
 	}
 
-	return purchases.Report{
+	return report.Report{
 		Items:    items,
-		FromDate: report.FromDate,
+		FromDate: r.FromDate,
 	}, nil
 }
